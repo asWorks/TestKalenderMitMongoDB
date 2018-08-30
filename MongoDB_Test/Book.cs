fbe8ac707cae715492b11a1223b77495e7c4a4cc
Notebook_As_Neu <dev@asWorks.de>
2018-08-30 05:57:46 +0200

    Initial Commit after Basic Implementation

---
+++ MongoDB_Test/Book.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB_Test
+{
+    public class Book
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("title")]  
+        public string Title { get; set; }
+        [BsonElement("price")]
+        public double Price { get; set; }
+
+        public Book(string title,double price)
+        {
+            Title = title;
+            Price = price;
+        }
+    }
+}
--- MongoDB_Test/MainWindow.xaml.cs
+++ MongoDB_Test/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,59 @@ namespace MongoDB_Test
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static MongoClient client = new MongoClient();
+        static IMongoDatabase db = client.GetDatabase("TestWPFDB");
+        static IMongoCollection<Book> collection = db.GetCollection<Book>("books");
+
+        public void ReadAllDocuments()
+        {
+            List<Book> list = collection.AsQueryable().ToList<Book>();
+            dgBook.ItemsSource = list;
+            Book b = (Book)dgBook.Items.GetItemAt(0);
+            tbId.Text = b.Id.ToString();
+            tbTitle.Text = b.Title;
+            tbPrice.Text = b.Price.ToString();
+
+        }
         public MainWindow()
         {
             InitializeComponent();
+            ReadAllDocuments();
+        }
+
+        private void dgBook_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Book b = (Book)dgBook.SelectedItem;
+            if (b != null)
+            {
+                tbId.Text = b.Id.ToString();
+                tbTitle.Text = b.Title;
+                tbPrice.Text = b.Price.ToString();
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Book b = new Book(tbTitle.Text, Double.Parse(tbPrice.Text));
+            collection.InsertOne(b);
+            ReadAllDocuments();
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var updateDef = Builders<Book>.Update.Set("title", tbTitle.Text)
+                .Set("price", tbPrice.Text);
+            collection.UpdateOne(b => b.Id == ObjectId.Parse(tbId.Text), updateDef);
+            ReadAllDocuments();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            collection.DeleteOne(b => b.Id == ObjectId.Parse(tbId.Text));
+            ReadAllDocuments();
         }
     }
 }
